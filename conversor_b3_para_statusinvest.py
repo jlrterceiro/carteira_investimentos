739ef941f02a5f2a3d6813bd764b46b3b36ef616
Terceiro <jlrterceiro@gmail.com>
2022-04-09 19:35:08 -0700

    mais coisas

--- conversor_b3_para_statusinvest.py
+++ conversor_b3_para_statusinvest.py
@@ -1,10 +1,6 @@
 import pandas as pd
-import locale
-
-locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
 inp = pd.read_excel('movimentacao-2022-04-09-17-39-48.xlsx')
-
 print(inp)
 
 dic = {}
@@ -26,32 +22,22 @@ for i in range(len(inp)) :
     if movimentacao not in ['Transferência - Liquidação', 'Compra', 'Venda'] :
         continue
 
-    data = inp.iloc[i, 1]
+    data_operacao = inp.iloc[i, 1]
     codigo_ativo = (inp.iloc[i, 3].split('-'))[0].strip()
-    
     categoria = ''
-    if codigo_ativo == 'TAEE11' :
+    if codigo_ativo == 'TAEE11' or not codigo_ativo.endswith('11') :
          categoria = 'Ações'
     elif codigo_ativo.startswith('Tesouro') :
         categoria = 'Tesouro Direto'
-    elif codigo_ativo.endswith('11') :
-        categoria = 'FII''s'
     else :
-        categoria = 'Ações'
-
-
+        categoria = 'FII''s'
     entrada_saida = inp.iloc[i, 0]
     operacao = 'C' if  entrada_saida == 'Credito' else 'V'
-
     quantidade = inp.iloc[i, 5]
-
     preco_unitario = str(inp.iloc[i, 6]).replace('.', ',')
-
     corretora = 'NU INVEST CORRETORA DE VALORES'
 
-    
-
-    dic['Data operação'].append(data)
+    dic['Data operação'].append(data_operacao)
     dic['Categoria'].append(categoria)
     dic['Código Ativo'].append(codigo_ativo)
     dic['Operação C/V'].append(operacao)
@@ -63,11 +49,8 @@ for i in range(len(inp)) :
     dic['Impostos'].append('')
     dic['IRRF'].append('')
     
-
+    
 out = pd.DataFrame(dic)
-
-print(out)
-
 out.to_excel('saida_terceiro.xlsx')
 
 
